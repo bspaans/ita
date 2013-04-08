117a14d9a7ea93e02b3a4c1a09f3cc2ef20071b7
Bart Spaans <bspaans@bsnb>
2013-04-08 18:15:03 +0200

    BS [Chapter 10.2]: Singly and doubly linked lists

--- ita_part3.js
+++ ita_part3.js
@@ -51,3 +51,107 @@ var Queue = function(size) {
 
     return self;
 };
+
+
+var LinkedListHead = function() {
+    this.item = null;
+    return this;
+}
+
+var LinkedList = function(key, head) {
+
+    var self = this;
+    self.key = key == undefined ? null : key;
+    self.headPointer = null;
+    self.next = null;
+    
+    self.head = function() { 
+        return self.headPointer == null ? null : self.headPointer.item; 
+    }
+
+    self.updateHead = function(newHead) {
+        self.headPointer = self.headPointer == null ? new LinkedListHead() : self.headPointer;
+        self.headPointer.item = newHead;
+    }
+
+    self.insert = function(elem) {
+        var x = new LinkedList(elem);
+        x.next = self.head();
+        x.next = x.next == null ? self : x.next;
+        self.updateHead(x);
+        x.headPointer = self.headPointer;
+        return x;
+    }
+
+    self.search = function(elem) {
+        var x = self.head();
+        while (x != null && x.key != elem) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    self.delete = function(elem) {
+        var x = self.search(elem);
+        return self.deleteLinkedList(x);
+    }
+
+    self.deleteLinkedList = function(x) {
+        var elem = self.head();
+        if (elem == x) {
+            self.updateHead(elem.next);
+            return 1;
+        }
+        var prev = null;
+        while (elem != null) {
+            if (elem == x) {
+                prev.next = elem.next;
+                return 1;
+            } 
+            prev = elem;
+            elem = elem.next;
+        }
+        return 0;
+    }
+
+    return self;
+};
+
+var DoublyLinkedList = function(x) {
+
+    var self = new LinkedList(x);
+    self.prev = null;
+
+    self.insert = function(elem) {
+        var x = new DoublyLinkedList(elem);
+        if (self.head() != null) {
+            x.next = self.head();
+            self.head().prev = x;
+        } else {
+            x.next = self;
+            self.prev = x;
+        }
+        self.updateHead(x);
+        x.headPointer = self.headPointer;
+        return x;
+    }
+
+    self.delete = function(elem) {
+        var x = self.search(elem);
+        if (x == null) {
+            return 0;
+        }
+
+        if (x.prev != null) {
+            x.prev.next = x.next;
+        } else {
+            self.updateHead(x.next);
+        }
+        if (x.next != null) {
+            x.next.prev = x.prev;
+        }
+        return 1;
+    }
+
+    return self;
+}
--- ita_part3_tests.js
+++ ita_part3_tests.js
@@ -67,3 +67,135 @@ test("I can dequeue an item from a Queue", function() {
     equal(queue.dequeue(), undefined);
 });
 
+
+module("Introduction to Algorithms - Part III - chapter 10.2");
+
+test("I can insert a value into a linked list, and keep head and next attributes in a correct state", function() {
+
+    var ll = new LinkedList();
+    equal(ll.key, null);
+    equal(ll.next, null);
+
+    var item1 = ll.insert(5);
+    equal(item1.head(), item1);
+    equal(ll.head(), item1);
+    equal(item1.key, 5);
+    equal(item1.next.key, null);
+    
+    var item2 = ll.insert(6);
+    equal(item2.head(), item2);
+    equal(item1.head(), item2);
+    equal(ll.head(), item2);
+
+    equal(item2.key, 6);
+    equal(item2.next.key, 5);
+    equal(item2.next.next.key, null);
+});
+
+test("I can insert on any instance of a singly linked list, but the element will always be added at the head", function() {
+
+    var ll = new LinkedList();
+    equal(ll.key, null);
+    var item1 = ll.insert(5);
+    var item2 = item1.insert(6);
+    equal(item2.head(), item2);
+    equal(item1.head(), item2);
+    equal(ll.head(), item2);
+
+    equal(item2.key, 6);
+    equal(item2.next.key, 5);
+    equal(item2.next.next.key, null);
+});
+
+test("I can search for a key in a singly linked list", function() {
+
+    var ll = new LinkedList(5);
+    var item1 = ll.insert(6);
+    var item2 = ll.insert(7);
+
+    equal(item2.search(5), ll);
+    equal(item1.search(7), item2);
+    equal(ll.search(6), item1);
+});
+
+test("If I search for an item that doesn't exist, I get a null value", function() {
+
+    var ll = new LinkedList(5);
+    var item1 = ll.insert(6);
+    var item2 = ll.insert(7);
+
+    equal(item2.search(9000), null);
+});
+
+test("I can delete an item from a linked list", function() {
+
+    var ll = new LinkedList(5);
+    ll.insert(6);
+    equal(ll.delete(5), 1);
+    equal(ll.search(5), null);
+    equal(ll.delete(5), 0);
+    equal(ll.delete(6), 1);
+    equal(ll.search(6), null);
+});
+
+test("I can insert a value into a doubly linked list, and keep head, next and prev attributes in a correct state", function() {
+
+    var ll = new DoublyLinkedList();
+    equal(ll.key, null);
+    equal(ll.next, null);
+    equal(ll.prev, null);
+
+    var item1 = ll.insert(5);
+    equal(ll.head(), item1);
+    equal(ll.key, null);
+    equal(ll.next, null);
+    equal(ll.prev, item1);
+
+    equal(item1.head(), item1);
+    equal(item1.key, 5);
+    equal(item1.next, ll);
+    equal(item1.prev, null);
+    
+    var item2 = ll.insert(6);
+    equal(item2.head(), item2);
+    equal(item1.head(), item2);
+    equal(ll.head(), item2);
+    
+    equal(item1.next, ll);
+    equal(item1.prev, item2);
+
+    equal(item2.key, 6);
+    equal(item2.next, item1);
+    equal(item2.prev, null);
+});
+
+test("I can search for a key in a doubly linked list", function() {
+
+    var ll = new DoublyLinkedList(5);
+    var item1 = ll.insert(6);
+    var item2 = ll.insert(7);
+
+    equal(item2.search(5), ll);
+    equal(item1.search(7), item2);
+    equal(ll.search(6), item1);
+});
+
+test("If I search for an item that doesn't exist, I get a null value", function() {
+
+    var ll = new DoublyLinkedList(5);
+    var item1 = ll.insert(6);
+    var item2 = ll.insert(7);
+
+    equal(item2.search(9000), null);
+});
+
+test("I can delete an item from a doubly linked list", function() {
+
+    var ll = new DoublyLinkedList(5);
+    ll.insert(6);
+    equal(ll.delete(5), 1);
+    equal(ll.search(5), null);
+    equal(ll.delete(5), 0);
+    equal(ll.delete(6), 1);
+    equal(ll.search(6), null);
+});
